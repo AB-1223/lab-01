15d84b9f00a9dac717b36b3c5ff632bc964a0fb5
Abhirami Binesh <binesh@ualberta.ca>
2026-01-07 18:39:31 -0700

    Add files via upload

---
+++ petshop/Happymood.java
@@ -0,0 +1,19 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happymood extends Mood{
+    public Happymood() {
+    }
+
+    public Happymood(Date date) {
+        super(date);
+    }
+
+    @Override
+    public String method() {
+        return "";
+    }
+
+
+}
---
+++ petshop/Sadmood.java
@@ -0,0 +1,18 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+    public class Sadmood extends Mood {
+    public Sadmood() {
+    }
+
+    public Sadmood(Date date) {
+        super(date);
+    }
+
+        @Override
+        public String method() {
+            return "";
+        }
+
+    }
